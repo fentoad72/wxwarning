ef2cbd0d64c261d10409c47f636bb2940b27f11a
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2021-11-24 10:12:15 -0700

    changed download path from 'current_all' to 'downloads'

--- wxwarning.py
+++ wxwarning.py
@@ -47,7 +47,7 @@ STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / "static"
 st.write(STREAMLIT_STATIC_PATH)
 # We create a downloads directory within the streamlit static asset directory
 # and we write output files to it
-DOWNLOADS_PATH = STREAMLIT_STATIC_PATH / "current_all"
+DOWNLOADS_PATH = STREAMLIT_STATIC_PATH / "downloads"
 if not DOWNLOADS_PATH.is_dir():
     DOWNLOADS_PATH.mkdir()
 
